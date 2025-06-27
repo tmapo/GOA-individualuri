00c8ad86a333446f992246a20be9f5eb3d854197
tmapo <asanidzeapolon732@gmail.com>
2025-06-27 14:34:16 +0400

    first commit

---
+++ day19/classwork/classwork2.py
@@ -0,0 +1,12 @@
+#classwork 1)
+# def manual_replace(text):
+#     result = ""
+#     for i in text:
+#         if i == " ":
+#             result += "-"
+#         else:
+#             result += i
+#     return result
+
+# print(manual_replace("Me miyvars restorani iberia"))
+
